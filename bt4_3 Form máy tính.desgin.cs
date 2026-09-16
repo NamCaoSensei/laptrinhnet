6f1c24727e15b0bb09c4ba04c5988e39265868fd
NamCao <nhnam2k6@gmail.com>
2026-09-16 11:07:09 +0700

    Update bt4_3 Form máy tính.desgin.cs

--- bt4_3 Form máy tính.desgin.cs
+++ bt4_3 Form máy tính.desgin.cs
@@ -1,1 +1,276 @@
+namespace bai4_3_form_may_tinh
+{
+    partial class Form1
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            txtDisplay = new TextBox();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            btnDiv = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            btnMul = new Button();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btnSub = new Button();
+            btn0 = new Button();
+            btnClear = new Button();
+            btnEqual = new Button();
+            btnAdd = new Button();
+            SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            txtDisplay.Font = new Font("Segoe UI", 18F);
+            txtDisplay.Location = new Point(12, 12);
+            txtDisplay.Name = "txtDisplay";
+            txtDisplay.ReadOnly = true;
+            txtDisplay.Size = new Size(260, 47);
+            txtDisplay.TabIndex = 0;
+            txtDisplay.Text = "0";
+            txtDisplay.TextAlign = HorizontalAlignment.Right;
+            // 
+            // btn7
+            // 
+            btn7.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btn7.Location = new Point(12, 65);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(60, 50);
+            btn7.TabIndex = 1;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += NumberButton_Click;
+            // 
+            // btn8
+            // 
+            btn8.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btn8.Location = new Point(78, 65);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(60, 50);
+            btn8.TabIndex = 2;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += NumberButton_Click;
+            // 
+            // btn9
+            // 
+            btn9.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btn9.Location = new Point(144, 65);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(60, 50);
+            btn9.TabIndex = 3;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += NumberButton_Click;
+            // 
+            // btnDiv
+            // 
+            btnDiv.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnDiv.Location = new Point(212, 65);
+            btnDiv.Name = "btnDiv";
+            btnDiv.Size = new Size(60, 50);
+            btnDiv.TabIndex = 4;
+            btnDiv.Text = "/";
+            btnDiv.UseVisualStyleBackColor = true;
+            btnDiv.Click += Operator_Click;
+            // 
+            // btn4
+            // 
+            btn4.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btn4.Location = new Point(12, 121);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(60, 50);
+            btn4.TabIndex = 5;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += NumberButton_Click;
+            // 
+            // btn5
+            // 
+            btn5.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btn5.Location = new Point(78, 121);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(60, 50);
+            btn5.TabIndex = 6;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += NumberButton_Click;
+            // 
+            // btn6
+            // 
+            btn6.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btn6.Location = new Point(144, 121);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(60, 50);
+            btn6.TabIndex = 7;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += NumberButton_Click;
+            // 
+            // btnMul
+            // 
+            btnMul.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnMul.Location = new Point(212, 121);
+            btnMul.Name = "btnMul";
+            btnMul.Size = new Size(60, 50);
+            btnMul.TabIndex = 8;
+            btnMul.Text = "*";
+            btnMul.UseVisualStyleBackColor = true;
+            btnMul.Click += Operator_Click;
+            // 
+            // btn1
+            // 
+            btn1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btn1.Location = new Point(12, 177);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(60, 50);
+            btn1.TabIndex = 9;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += NumberButton_Click;
+            // 
+            // btn2
+            // 
+            btn2.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btn2.Location = new Point(78, 177);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(60, 50);
+            btn2.TabIndex = 10;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += NumberButton_Click;
+            // 
+            // btn3
+            // 
+            btn3.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btn3.Location = new Point(144, 177);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(60, 50);
+            btn3.TabIndex = 11;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += NumberButton_Click;
+            // 
+            // btnSub
+            // 
+            btnSub.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnSub.Location = new Point(212, 177);
+            btnSub.Name = "btnSub";
+            btnSub.Size = new Size(60, 50);
+            btnSub.TabIndex = 12;
+            btnSub.Text = "-";
+            btnSub.UseVisualStyleBackColor = true;
+            btnSub.Click += Operator_Click;
+            // 
+            // btn0
+            // 
+            btn0.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btn0.Location = new Point(12, 233);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(60, 50);
+            btn0.TabIndex = 13;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += NumberButton_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnClear.Location = new Point(78, 233);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(60, 50);
+            btnClear.TabIndex = 14;
+            btnClear.Text = "C";
+            btnClear.UseVisualStyleBackColor = true;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnEqual
+            // 
+            btnEqual.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnEqual.Location = new Point(144, 233);
+            btnEqual.Name = "btnEqual";
+            btnEqual.Size = new Size(60, 50);
+            btnEqual.TabIndex = 15;
+            btnEqual.Text = "=";
+            btnEqual.UseVisualStyleBackColor = true;
+            btnEqual.Click += btnEqual_Click;
+            // 
+            // btnAdd
+            // 
+            btnAdd.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            btnAdd.Location = new Point(212, 233);
+            btnAdd.Name = "btnAdd";
+            btnAdd.Size = new Size(60, 50);
+            btnAdd.TabIndex = 16;
+            btnAdd.Text = "+";
+            btnAdd.UseVisualStyleBackColor = true;
+            btnAdd.Click += Operator_Click;
+            // 
+            // Form1
+            // 
+            ClientSize = new Size(284, 296);
+            Controls.Add(btnAdd);
+            Controls.Add(btnEqual);
+            Controls.Add(btnClear);
+            Controls.Add(btn0);
+            Controls.Add(btnSub);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(btnMul);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btnDiv);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(txtDisplay);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Calculator";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btnDiv;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btnMul;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnSub;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnEqual;
+        private System.Windows.Forms.Button btnAdd;
+    }
+}
 
